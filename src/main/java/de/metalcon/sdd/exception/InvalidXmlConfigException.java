23232066156c20898c69131481bcd2683c80b903
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-03-21 17:55:34 +0100

    Implemented XmlConfig.

---
+++ src/main/java/de/metalcon/sdd/exception/InvalidXmlConfigException.java
@@ -0,0 +1,12 @@
+package de.metalcon.sdd.exception;
+
+public class InvalidXmlConfigException extends InvalidConfigException {
+
+    private static final long serialVersionUID = -7336759329795812876L;
+
+    public InvalidXmlConfigException(
+            String message) {
+        super(message);
+    }
+
+}
