865e1b6cdce33f301cdeffe147f1a84280f1abb0
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-03-31 11:42:50 +0200

    Implememted RequestHandler.

---
+++ src/main/java/de/metalcon/sdd/RequestHandler.java
@@ -0,0 +1,100 @@
+package de.metalcon.sdd;
+
+import de.metalcon.api.responses.Response;
+import de.metalcon.api.responses.errors.InternalServerErrorResponse;
+import de.metalcon.api.responses.errors.UsageErrorResponse;
+import de.metalcon.sdd.api.exception.SddInternalServerException;
+import de.metalcon.sdd.api.exception.SddUsageException;
+import de.metalcon.sdd.api.requests.AddRelationsAction;
+import de.metalcon.sdd.api.requests.DeleteAction;
+import de.metalcon.sdd.api.requests.DeleteRelationsAction;
+import de.metalcon.sdd.api.requests.ReadRequestAction;
+import de.metalcon.sdd.api.requests.SddReadRequest;
+import de.metalcon.sdd.api.requests.SddWriteRequest;
+import de.metalcon.sdd.api.requests.SetPropertiesAction;
+import de.metalcon.sdd.api.requests.SetRelationAction;
+import de.metalcon.sdd.api.requests.SetRelationsAction;
+import de.metalcon.sdd.api.requests.WriteRequestAction;
+import de.metalcon.sdd.api.responses.SddCouldNotQueueResponse;
+import de.metalcon.sdd.api.responses.SddSucessfulReadResponse;
+import de.metalcon.sdd.api.responses.SddSucessfullQueueResponse;
+import de.metalcon.zmqworker.ZMQRequestHandler;
+
+public class RequestHandler implements ZMQRequestHandler {
+
+    private Sdd sdd;
+
+    public RequestHandler(
+            Sdd sdd) {
+        this.sdd = sdd;
+    }
+
+    @Override
+    public Response handleRequest(Object request) {
+        try {
+            if (request instanceof SddReadRequest) {
+                handleReadRequest((SddReadRequest) request);
+            } else if (request instanceof SddWriteRequest) {
+                handleWriteRequest((SddWriteRequest) request);
+            }
+        } catch (SddInternalServerException e) {
+            return new InternalServerErrorResponse(e, null);
+        } catch (SddUsageException e) {
+            return new UsageErrorResponse(e, null);
+        } catch (RuntimeException e) {
+            return new UsageErrorResponse(e, null);
+        }
+
+        return new UsageErrorResponse("unknown request type",
+                "use requests defined in staticDataDeliveryApi");
+    }
+
+    private Response handleReadRequest(SddReadRequest request) {
+        SddSucessfulReadResponse response = new SddSucessfulReadResponse();
+        for (ReadRequestAction read : request.getActions()) {
+            long nodeId = read.getNodeId();
+            String detail = read.getDetail();
+            String output = sdd.read(nodeId, detail);
+            response.add(nodeId, detail, output);
+        }
+        return response;
+    }
+
+    private Response handleWriteRequest(SddWriteRequest request) {
+        WriteTransaction tx = sdd.createWriteTransaction();
+        for (WriteRequestAction write : request.getActions()) {
+            if (write instanceof SetPropertiesAction) {
+                SetPropertiesAction w = (SetPropertiesAction) write;
+                tx.setProperties(w.getNodeId(), w.getNodeType(),
+                        w.getProperties());
+            } else if (write instanceof SetRelationAction) {
+                SetRelationAction w = (SetRelationAction) write;
+                tx.setRelation(w.getNodeId(), w.getNodeType(), w.getRelation(),
+                        w.getToId());
+            } else if (write instanceof SetRelationsAction) {
+                SetRelationsAction w = (SetRelationsAction) write;
+                tx.setRelations(w.getNodeId(), w.getNodeType(),
+                        w.getRelation(), w.getToIds());
+            } else if (write instanceof AddRelationsAction) {
+                AddRelationsAction w = (AddRelationsAction) write;
+                tx.addRelations(w.getNodeId(), w.getNodeType(),
+                        w.getRelation(), w.getToIds());
+            } else if (write instanceof DeleteAction) {
+                DeleteAction w = (DeleteAction) write;
+                tx.delete(w.getNodeId());
+            } else if (write instanceof DeleteRelationsAction) {
+                DeleteRelationsAction w = (DeleteRelationsAction) write;
+                tx.deleteRelations(w.getNodeId(), w.getNodeType(),
+                        w.getRelation(), w.getToIds());
+            } else {
+                throw new IllegalStateException("Unkown WriteRequestAction.");
+            }
+        }
+        if (tx.commit()) {
+            return new SddSucessfullQueueResponse();
+        } else {
+            return new SddCouldNotQueueResponse();
+        }
+    }
+
+}
