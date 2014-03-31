86ea96037c81b5b8110057f645fe66208a32e1cc
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-03-31 14:40:25 +0200

    Now proably closing all resources on shutdown.

--- src/main/java/de/metalcon/sdd/StaticDataDelivery.java
+++ src/main/java/de/metalcon/sdd/StaticDataDelivery.java
@@ -11,26 +11,73 @@ import de.metalcon.sdd.api.requests.SddRequest;
 import de.metalcon.sdd.config.Config;
 import de.metalcon.sdd.config.XmlConfig;
 
-public class StaticDataDelivery {
+public class StaticDataDelivery implements AutoCloseable {
 
-    public static void main(String[] args) throws IOException {
+    private Sdd sdd;
+
+    private ZMQ.Context context;
+
+    private ZmqWorker<SddRequest, Response> worker;
+
+    public StaticDataDelivery() throws IOException {
+        super();
+
         System.out.println("Loading config...");
         Config config = new XmlConfig("/usr/share/sdd/config.xml");
         System.out.println("Starting Sdd...");
-        Sdd sdd = new Sdd(config);
+        sdd = new Sdd(config);
 
         System.out.println("Creating Context...");
-        ZMQ.Context context = ZMQ.context(1);
-
+        context = ZMQ.context(1);
         System.out.println("Creating Worker...");
-        ZmqWorker<SddRequest, Response> worker =
-                new ZmqWorker<>(context, "tcp://127.0.0.1:1337",
-                        new RequestHandler(sdd));
+        worker =
+                new ZmqWorker<SddRequest, Response>(context,
+                        "tcp://127.0.0.1:1337", new RequestHandler(sdd));
 
-        System.out.println("Starting Worker...");
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    close();
+                } catch (Exception e) {
+                    // Ignore Exceptions we are shutting down
+                }
+            }
+        });
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Closing Worker...");
+        if (worker != null) {
+            worker.close();
+            worker = null;
+        }
+        System.out.println("Closing Context...");
+        if (context != null) {
+            context.close();
+            context = null;
+        }
+
+        System.out.println("Closing Sdd...");
+        if (sdd != null) {
+            sdd.close();
+            sdd = null;
+        }
+    }
+
+    public void run() {
+        System.out.println("Starting worker...");
         worker.start();
 
         System.out.println("Ready!");
+    }
+
+    public static void main(String[] args) throws IOException {
+        @SuppressWarnings("resource")
+        StaticDataDelivery main = new StaticDataDelivery();
+        main.run();
     }
 
 }
